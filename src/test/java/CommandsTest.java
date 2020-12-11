ce9521ef1ee82326674b29d8e6cd428b3fd6f644
MonolithOfficial <cotneaburjania@gmail.com>
2020-12-11 20:33:58 +0400

    Finished homework (added isDisplayed() probability)

--- src/test/java/CommandsTest.java
+++ src/test/java/CommandsTest.java
@@ -42,9 +42,20 @@ public class CommandsTest {
         // Catching Enable button, clicking it and waiting until "It's enabled!" text is displayed and the
         // input is enabled.
         driver.findElement(By.xpath("/html/body/div[2]/div/div[1]/form[2]/button")).click();
+
+        // If the task is about isDisplayed()
+//        if (driver.findElement(By.xpath("/html/body/div[2]/div/div[1]/form[2]/input")).isEnabled()
+//                && driver.findElement(By.id("message")).isDisplayed()){
+//            System.out.println("Check passed.");
+//        }
+//        else {
+//            System.out.println("Check failed");
+//        }
+
+        // if the task ain't about isDisplayed() and we wanna SEE the text.
         Thread.sleep(5000);
 
-        // Checking for validity.
+        // Checking for validity .
         if (driver.findElement(By.xpath("/html/body/div[2]/div/div[1]/form[2]/input")).isEnabled()
             && driver.findElement(By.id("message")).getText().equals("It's enabled!")){
             System.out.println("Check passed.");
